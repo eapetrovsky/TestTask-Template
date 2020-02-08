c5c3a9ad09ecd48c621c7a7e09bcc883687a9172
eapetrovsky <eapetrovsky@users.noreply.github.com>
2020-02-08 13:24:14 +0200

    Initial commit

---
+++ CSharpTest/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime startDate = new DateTime(2017, 4, 21);
+            int dayCount = 7;
+            WeekEnd[] weekends = new WeekEnd[2]
+            {
+                new WeekEnd(new DateTime(2017, 4, 23), new DateTime(2017, 4, 25)),
+                new WeekEnd(new DateTime(2017, 4, 29), new DateTime(2017, 4, 29))
+            };
+             
+            WorkDayCalculator wdk = new WorkDayCalculator();
+            wdk.Calculate(startDate, dayCount, weekends);
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ CSharpTest/WorkDayCalculator.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpTest
+{
+    public class WorkDayCalculator : IWorkDayCalculator
+    {
+        public DateTime Calculate(DateTime startDate, int dayCount, WeekEnd[] weekEnds)
+        { 
+            if (weekEnds == null)
+            {
+                return startDate.AddDays(-1);
+            } 
+
+            int day = 1;
+
+            int totalDaysQty;
+            
+            if (weekEnds.GetLength(0) > 1)
+            {
+                totalDaysQty = (int)(weekEnds[1].EndDate.ToOADate() - weekEnds[1].StartDate.ToOADate() + 1 +
+                    weekEnds[1].EndDate.ToOADate() - weekEnds[1].StartDate.ToOADate() + dayCount + 2);
+            }
+            else
+            {
+                totalDaysQty = (int)(weekEnds[0].EndDate.ToOADate() - weekEnds[0].StartDate.ToOADate() + dayCount + 1);
+            }
+             
+            DateTime[] newDT = new DateTime[totalDaysQty];
+            DateTime variableDay = newDT[0] = startDate;
+            variableDay = variableDay.AddDays(1); 
+
+            int firstWeekendLength = (int)(weekEnds[0].EndDate.ToOADate() - weekEnds[0].StartDate.ToOADate() + 1);
+                        
+            int secondtWeekendLength = 0;
+                          
+            if (weekEnds.GetLength(0) > 1)
+            { 
+                secondtWeekendLength = (int)(weekEnds[1].EndDate.ToOADate() - weekEnds[1].StartDate.ToOADate() + 1);           
+            }       
+
+            Console.WriteLine("Start date:" + newDT[0].ToString());
+            Console.WriteLine("Duration: {0} days", dayCount);
+            Console.WriteLine("Day  " + day.ToString() + ":\t" + newDT[0].ToString("dd.MM.yyyy"));
+
+            for (int i = 0; i < totalDaysQty; i++)
+            {
+                if (variableDay != weekEnds[0].StartDate)
+                {
+                    newDT[i] = variableDay;
+                    variableDay = variableDay.AddDays(1);
+                    day++;
+                    Console.WriteLine("Day  " + day.ToString() + ":\t" + newDT[i].ToString("dd.MM.yyyy"));                    
+                }
+                else if (weekEnds[0].StartDate.CompareTo(variableDay) == 0)
+                {
+                    for (int j = 0; j < firstWeekendLength; j++)
+                    {                        
+                        newDT[i] = variableDay;
+                        Console.WriteLine(newDT[i].ToString("dd.MM.yyyy") + "   excluded as weekend ");
+                        i++;
+                        variableDay = variableDay.AddDays(1);                        
+                    }                     
+                }
+                else if (secondtWeekendLength > 0 && weekEnds[1].StartDate.CompareTo(variableDay) == 0)
+                {
+                    for (int j = 0; j < secondtWeekendLength; j++)
+                    {
+                        variableDay = variableDay.AddDays(1);
+                        newDT[i] = weekEnds[1].StartDate;
+                        i++;
+                        day++;
+                        Console.WriteLine(newDT[i].ToString("dd.MM.yyyy") + "   excluded as weekend ");
+                    }  
+                }                
+            }
+             
+            return newDT[newDT.Length - 1];
+        }
+    }
+}
